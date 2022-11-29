790e9534170a13d1c72409f40ffd2623089dc1f8
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2022-11-29 10:44:00 +0100

    Priprema zavrsnog rada
    Kreirane tablice sa vezama na bazi podataka proizvodnje neke tvrtke koja se bavi proizvodnjom

---
+++ SQL/Proizvodnja/proizvodnja.sql
@@ -0,0 +1,95 @@
+drop database if exists proizvodnja;
+create database proizvodnja;
+use proizvodnja;
+
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\djuki\Documents\GitHub\UcenjePHP\SQL\Proizvodnja\proizvodnja.sql
+
+create table osoba(
+    id int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11)
+);
+
+create table radnik(
+    id int not null primary key auto_increment,
+    brojugovora varchar(50),
+    iban varchar(50),
+    osoba int not null
+);
+
+create table menadzment(
+    id int not null primary key auto_increment,
+    brojugovora varchar(50),
+    iban varchar(50),
+    osoba int,
+    nadredjeni int
+);
+
+create table smjena(
+    id int not null primary key auto_increment,
+    naziv varchar(50),
+    menadzment int
+);
+
+create table radnik_smjena(
+    radnik int,
+    smjena int
+);
+
+create table proizvod(
+    id int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    boja varchar(50),
+    cijena decimal(18,2),
+    smjena int,
+    skladiste int
+);
+
+create table skladiste(
+    id int not null primary key auto_increment,
+    naziv varchar(50),
+    skladisnikapacitet int
+);
+
+create table narudzba(
+    id int not null primary key auto_increment,
+    brojnarudzbe varchar(50),
+    klijent int,
+    skladiste int
+);
+
+create table klijent(
+    id int not null primary key auto_increment,
+    nazivtvrtke varchar(50),
+    sjediste varchar(50),
+    adresa varchar (100)
+);
+
+create table posiljka(
+    id int not null primary key auto_increment,
+    brojposiljke varchar(50),
+    klijent int,
+    skladiste int
+);
+
+#poveznice izmedju tablica
+
+alter table radnik add foreign key (osoba) references osoba(id);
+
+alter table menadzment add foreign key(osoba) references osoba(id);
+alter table menadzment add foreign key (nadredjeni)references menadzment(id);
+
+alter table radnik_smjena add foreign key (radnik) references radnik(id);
+alter table radnik_smjena add foreign key (smjena) references smjena(id);
+
+alter table smjena add foreign key (menadzment) references menadzment(id);
+
+alter table proizvod add foreign key (smjena) references smjena(id);
+alter table proizvod add foreign key (skladiste) references skladiste(id);
+
+alter table narudzba add foreign key (skladiste) references skladiste(id);
+alter table narudzba add foreign key (klijent) references klijent(id);
+
+alter table posiljka add foreign key (skladiste) references skladiste (id);
+alter table posiljka add foreign key (klijent) references klijent (id);
