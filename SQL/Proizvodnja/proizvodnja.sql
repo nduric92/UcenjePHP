2fc56cb4a93edbc0b4fc7ecf00f07dee52e3f2a1
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2022-12-08 13:45:37 +0100

    proizvodnja last commit
    Reorganizacija tablica te njihovih poveznica

--- SQL/Proizvodnja/proizvodnja.sql
+++ SQL/Proizvodnja/proizvodnja.sql
@@ -4,26 +4,13 @@ use proizvodnja;
 
 # c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\djuki\Documents\GitHub\UcenjePHP\SQL\Proizvodnja\proizvodnja.sql
 
-create table osoba(
+create table radnik(
     id int not null primary key auto_increment,
     ime varchar(50) not null,
     prezime varchar(50) not null,
-    oib char(11)
-);
-
-create table radnik(
-    id int not null primary key auto_increment,
+    oib char (11),
     brojugovora varchar(50),
-    iban varchar(50),
-    osoba int not null
-);
-
-create table menadzment(
-    id int not null primary key auto_increment,
-    brojugovora varchar(50),
-    iban varchar(50),
-    osoba int,
-    nadredjeni int
+    iban varchar(50)
 );
 
 create table smjena(
@@ -33,30 +20,26 @@ create table smjena(
 );
 
 create table radnik_smjena(
+    id int not null primary key auto_increment,
     radnik int,
     smjena int
+);
+
+create table radnik_smjena_proizvod(
+    radnik_smjena int,
+    proizvod int,
+    kolicina int
 );
 
 create table proizvod(
     id int not null primary key auto_increment,
     naziv varchar(50) not null,
     boja varchar(50),
-    cijena decimal(18,2),
-    smjena int
+    cijena decimal(18,2)
 );
 
 
 #poveznice izmedju tablica
 
-alter table radnik add foreign key (osoba) references osoba(id);
-
-alter table menadzment add foreign key(osoba) references osoba(id);
-alter table menadzment add foreign key (nadredjeni)references menadzment(id);
-
 alter table radnik_smjena add foreign key (radnik) references radnik(id);
 alter table radnik_smjena add foreign key (smjena) references smjena(id);
-
-alter table smjena add foreign key (menadzment) references menadzment(id);
-
-alter table proizvod add foreign key (smjena) references smjena(id);
-
