810b2eec395e272ed50993dc435e15629428a9d4
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2022-12-20 10:41:36 +0100

    PHP prvi dio
    Ucenje php

---
+++ www/ucenjephp.hr/izbornik.php
@@ -0,0 +1,24 @@
+<div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+
+<div class="top-bar" id="example-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li>
+        <a href="index.php">
+        Ucenje PHP</a>
+        </li>
+      <li class="has-submenu">
+            <a href="#0">Suzivot HTML i PHP</a>
+        <ul class="submenu menu vertical" data-submenu>
+          <li><a href="helloworld.php">Hello world</a></li>
+          <li><a href="kombinacijahtmlphp.php">Kombinacija HTML i PHP</a></li>
+        </ul>
+      </li>
+      
+    </ul>
+  </div>
+  
+</div>
