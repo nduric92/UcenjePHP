cd5a0926f07b03d9184ea6a09db1c100fdac2343
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-13 11:04:23 +0100

    Update zadatak4.php

--- www/ucenjephp.hr/zadatak4.php
+++ www/ucenjephp.hr/zadatak4.php
@@ -6,17 +6,12 @@ $b3 = isset($_GET['b3']) ? $_GET['b3'] : 0;
 
 if($b1===$b2 && $b2===$b3){
     echo 'Svi brojevi su jednaki';    
-}else if($b1>$b2 && $b2>$b3){
-    echo $b3;
 }else if($b1<$b2 && $b2<$b3){
     echo $b1;
-}else if($b1>$b2 && $b2<$b3){
+}else if($b2<$b3){
     echo $b2;
-}else if($b1==$b2 && $b2<$b3){
-    echo $b2;
-}else if($b1>$b2 && $b2==$b3){
-    echo $b2;
-}else if($b1==$b3 && $b2>$b3){
-    echo $b1;
+}else{
+    echo $b3;
 }
 
+//ispisuje najmanji broj
