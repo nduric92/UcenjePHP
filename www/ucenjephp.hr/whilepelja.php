b927fe94734307d13e7a9238d48f98a71e938a82
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-17 15:11:41 +0100

    s predavanja

--- www/ucenjephp.hr/whilepelja.php
+++ www/ucenjephp.hr/whilepelja.php
@@ -1,28 +1,73 @@
 <?php
 
-//while radi s boolean tipom podatka
-
-$uvijet=true;
-$i = 1;
-
-while($uvijet){
-    echo $i++, '<br>';
-    if($i>10){
-        $uvijet=false;
+// while radi s boolean tipom podatka
+$uvjet=false;
+$t=1;
+while($uvjet){
+    echo $t++, '<br>';
+    if($t>10){
+        $uvjet=false;
     }
 }
-
 echo '<hr>';
 
-while($uvijet){
+// niti jednom neće ući u petlju
+$j=10;
+for($i=4;$i>$j;$i--){
     echo $i++, '<br>';
-    if($i>10){
-        $uvijet=false;
-    }
+}
+echo '&gt;' . $i; // varijabla i ima vrijednost koju je ostvarila u petlji
+echo '<hr>';
+
+// uobičajna while petlja
+$i=0;
+
+while($i<10){
+    echo ++$i, '<br>';
+}
+echo '<hr>';
+// continue isto kao u for
+// break isto kao i for
+
+// Zadatak: 
+//Za uneseni cijeli broj ispišite koliko ima znamenaka
+$broj=23234;
+$znamenaka=0;
+while($broj>0){
+    $znamenaka++;
+    $broj = (int)($broj/10);
+    echo $broj, '<br />';
 }
 
-//beskonacna while petlja
+echo $znamenaka;
+echo '<hr>';
 
+
+// beskonačna while petlja
 while(true){
-    break; //izlaz iz beskonacne petlje
+    break;
 }
+
+
+$datoteka='datoteka.txt';
+$najveci=0;
+$ukupno=0;
+if (file_exists($datoteka)) { // ako datoteka postoji
+    $dat = fopen($datoteka, 'r');
+    while (($line = fgets($dat)) !==false) {
+        //echo strlen($line), '<br>';
+        if(strlen($line)===2){ // dva su znaka skrivena: CR i LF
+            //echo $line, ',' . $ukupno, '<br>';
+            if($ukupno>$najveci){
+                $najveci=$ukupno;
+            }
+            $ukupno=0;
+        }else{
+            $ukupno+=(int)$line;
+        }
+       
+
+    }
+    fclose($dat);
+}
+echo $najveci;
--- www/ucenjephp.hr/zadatak5a.php
+++ www/ucenjephp.hr/zadatak5a.php
@@ -1,12 +1,13 @@
 <?php
 
-//increment decrement zadatak sa predavanja
-
 $i=2; $j=0; $k=1;
 
-$i += $k++ + --$j;
-$k = $i + $j; 
+$i += $k++ + --$j; // na ovoj linij desno 1 + -1   k=2, j=-1, i=2
+$k = $i + $j; // i=2, j=-1, k=1
 
-echo $i + --$j;
+echo $i + --$j; // i=2, j=-2
 
 
+for($i=0;$i<10;$i=$i+1){
+    echo 'Osijek', '<br>';
+}
