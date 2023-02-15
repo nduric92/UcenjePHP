a34be16ce60212ae4a633db200d82f6d1b57283a
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-02-15 13:14:04 +0100

    predavanje

---
+++ www/autorizacija.hr/autorizacija.php
@@ -0,0 +1,16 @@
+<?php
+
+// prvo će se odraditi kontrole treba li uopće ići
+// autorizirati
+
+// ide autorizacija u bazu ali sada radim fiksno
+if($_POST['email']==='edunova@edunova.hr' && 
+    $_POST['lozinka']==='edunova'){
+        session_start();
+        $_SESSION['auth']=true;
+        setcookie('email',$_POST['email']);
+        header('location: zasticeno.php');
+
+    }else{
+        header('location: index.php?email=' . $_POST['email']);
+    }
---
+++ www/autorizacija.hr/index.php
@@ -0,0 +1,30 @@
+<?php 
+//session_start();
+
+//echo '<pre>';
+//print_r($_COOKIE);
+//echo '</pre>';
+//$_SESSION['kljuc']='Hello';
+
+$email = isset($_GET['email']) ? $_GET['email'] : (isset($_COOKIE['email']) ? $_COOKIE['email'] : '');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javno</title>
+</head>
+<body>
+    Javni dio
+    <form action="autorizacija.php" method="post">
+        <input type="text" name="email"
+        value="<?=$email?>"
+        placeholder="email" id="">
+        <input type="password" name="lozinka" id="">
+        <input type="submit" value="Autoriziraj se">
+    </form>
+</body>
+</html>
