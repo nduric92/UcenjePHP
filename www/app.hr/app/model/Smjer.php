26e99620015b39aeeb7a9320d59b1a1e8418d6de
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-03-11 17:07:54 +0100

    prd

--- www/app.hr/app/model/Smjer.php
+++ www/app.hr/app/model/Smjer.php
@@ -17,4 +17,66 @@ class Smjer
         return $izraz->fetchAll();
     }
 
+    public static function readOne($sifra)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            select * from smjer
+            where sifra=:sifra
+        
+        ');
+        $izraz->execute([
+            'sifra'=>$sifra
+        ]);
+        return $izraz->fetch();
+    }
+
+    public static function create($parametri)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            insert into smjer(naziv,cijena,upisnina,
+            trajanje,certificiran) values
+            (:naziv,:cijena,:upisnina,
+            :trajanje,:certificiran);
+        
+        ');
+        $izraz->execute($parametri);
+    }
+
+    public static function update($parametri)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            update smjer set
+            naziv=:naziv,
+            cijena=:cijena,
+            upisnina=:upisnina,
+            trajanje=:trajanje,
+            certificiran=:certificiran
+            where sifra=:sifra
+        
+        ');
+        $izraz->execute($parametri);
+    }
+
+    public static function postojiIstiNazivUBazi($s)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare('
+        
+            select sifra from smjer
+            where naziv=:naziv
+        
+        ');
+        $izraz->execute([
+            'naziv'=>$s
+        ]);
+        $sifra=$izraz->fetchColumn();
+        return $sifra>0;
+    }
+
 }
